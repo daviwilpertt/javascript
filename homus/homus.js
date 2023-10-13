0490691bdacefb59aadb906ba26feff94adf5e68
davizok4 <daviwilpertt@proton.me>
2023-10-12 21:34:34 -0300

    b

--- homus/homus.js
+++ homus/homus.js
@@ -1,10 +1,9 @@
 let sabor1, preco_total;
 let saida1 = document.getElementById("saida1");
 let saida2 = document.getElementById("saida2");
-let preco_pizza;
-function pizza(){
+function pizza() {
     sabor1 = document.getElementById("porta");
-    preco_pizza = sabor1.value;
+    let preco_pizza = sabor1.value;
     switch (preco_pizza) {
         case "queijo": preco_pizza = 20;
             break;
@@ -23,24 +22,22 @@ function pizza(){
     let grande = document.getElementById("tam_grande");
     let familia = document.getElementById("tam_familia");
     if (broto.checked) {
-        saida1.innerHTML = "valor pizza broto R$" + preco_pizza.toFixed(2) ;
+        saida1.innerHTML = "valor pizza broto R$" + preco_pizza.toFixed(2);
     }
     else if (media.checked) {
         preco_pizza *= 1.5
-        saida1.innerHTML = "valor pizza média R$" + preco_pizza.toFixed(2) ;
+        saida1.innerHTML = "valor pizza média R$" + preco_pizza.toFixed(2);
     }
     else if (grande.checked) {
         preco_pizza *= 1.8
-        saida1.innerHTML = "valor pizza grande R$" + preco_pizza.toFixed(2) ;
+        saida1.innerHTML = "valor pizza grande R$" + preco_pizza.toFixed(2);
     }
     else if (familia.checked) {
         preco_pizza *= 2
-        saida1.innerHTML = "valor pizza família R$" + preco_pizza.toFixed(2) ;
-
-
+        saida1.innerHTML = "valor pizza família R$" + preco_pizza.toFixed(2);
     }
     else {
-
+        alert("erro")
     }
     let borda_salgada = document.getElementById("borda_salgada");
     let borda_doce = document.getElementById("borda_doce");
@@ -55,6 +52,4 @@ function pizza(){
     else {
         saida2.innerHTML = "valor da pizza sem borda " + preco_pizza.toFixed(2)
     }
-
-
 }
