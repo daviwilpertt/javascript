93e6fc184c1e82b122c3415d0350b3628dc1c3c4
Your Name <daviwilpertt@proton.me>
2023-10-17 22:06:48 -0300

    f

--- adivinhacao/adivinha.js
+++ adivinhacao/adivinha.js
@@ -1,12 +1,15 @@
 let numero_aleatorio = Math.random().toFixed(1) * 10
 console.log(numero_aleatorio)
 
+let tentativas = Number()
+let max_tentativas = Number()
 let saida = document.getElementById("saida")
 function resultado() {
     let entrada = document.getElementById("numero_digitado").value
     if (entrada > 0 && entrada <= 10) {
+        tentativas++
         if (entrada == numero_aleatorio) {
-            saida.innerHTML = "acertou"
+            saida.innerHTML = `acertou em ${tentativas} tentativas` 
         }
         else if (entrada > numero_aleatorio) {
             saida.innerHTML = "chute alto"
