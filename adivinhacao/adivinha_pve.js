069102e99f20ddef6ecf1fa6fa9e8cb9a650f300
Your Name <daviwilpertt@proton.me>
2023-10-23 21:59:50 -0300

    gf

--- adivinhacao/adivinha_pve.js
+++ adivinhacao/adivinha_pve.js
@@ -1,18 +1,24 @@
 let saida_resultado = document.getElementById("saida");
 let saida_dificuldade = document.getElementById("saida_dificuldade");
 let saida_tentativas = document.getElementById("tentativas_restantes");
-let facil = document.getElementById("facil");
-let media = document.getElementById("media");
-let dificil = document.getElementById("dificil");
-let mt_dificil = document.getElementById("mt_dificil");
-let numero_aleatorio = Math.random().toFixed(1) * 10;
-let tentativas = Number(1);
+let saida_pontuacao = document.getElementById("saida_pontuacao");
+let saida_max_pontuacao=document.getElementById("max_pontuacao")
+let numero_aleatorio = (Math.floor(Math.random() * 10 + 1));
+let tentativas = 1;
 let max_tentativas = Number();
+let pontuacao = 100;
+let pontuacao_rodada = pontuacao
+let pontuacao_max = 0 ;
 let resetar = document.getElementById("botao_resetar")
 let res = document.getElementById("botao_res");
 let dif = document.getElementById("botao_dif");
-//console.log(numero_aleatorio)
+console.log(numero_aleatorio)
+
 function dificuldade() {
+    let facil = document.getElementById("facil");
+    let media = document.getElementById("media");
+    let dificil = document.getElementById("dificil");
+    let mt_dificil = document.getElementById("mt_dificil");
     res.disabled = false
     dif.disabled = true
     if (facil.checked) {
@@ -41,27 +47,31 @@ function resultado() {
             saida_tentativas.innerHTML = "tentativas restantes: " + max_tentativas
         }
         else if (max_tentativas == 0) {
-            saida_tentativas.innerHTML = `acabaram as tentativas <br> o número era ${numero_aleatorio}` 
+            saida_tentativas.innerHTML = `acabaram as tentativas <br> o número era ${numero_aleatorio}`
             res.disabled = true
             resetar.disabled = false
             saida_resultado.innerHTML = "&nbsp"
         }
-        else {
-        }
+        else { }
         if (entrada == numero_aleatorio) {
             saida_resultado.innerHTML = `acertou em ${tentativas} tentativas`
             saida_tentativas.innerHTML = "jogo encerrado"
             res.disabled = true
             resetar.disabled = false
-
+            saida_max_pontuacao.innerHTML = pontuacao_max 
         }
         else if (entrada > numero_aleatorio) {
             saida_resultado.innerHTML = "número é menor"
-            tentativas++
+            tentativas++;
         }
         else if (entrada < numero_aleatorio) {
             saida_resultado.innerHTML = "número é maior"
-            tentativas++
+            tentativas++;
+        }
+        while (pontuacao_rodada > 0) {
+            if (pontuacao > pontuacao_max) {
+                pontu
+            }
         }
     }
     else {
@@ -71,8 +81,8 @@ function resultado() {
 
 function reset() {
     entrada = document.getElementById("numero_digitado")
-    numero_aleatorio = Math.random().toFixed(1) * 10
-    // console.log(numero_aleatorio)
+    numero_aleatorio = (Math.floor(Math.random() * 10 + 1));
+    console.log(numero_aleatorio)
     resetar.disabled = true
     dif.disabled = false
     saida_tentativas.innerHTML = "&nbsp"
@@ -80,4 +90,5 @@ function reset() {
     saida_resultado.innerHTML = "&nbsp"
     tentativas = 0
     tentativas++
+    saida_pontuacao.innerHTML = pontuacao_rodada = 100
 }
