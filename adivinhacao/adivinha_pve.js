30501fa7ea2c90a5d5cbd9477bd14e4e5db58606
Your Name <daviwilpertt@proton.me>
2023-10-24 22:04:41 -0300

    d

--- adivinhacao/adivinha_pve.js
+++ adivinhacao/adivinha_pve.js
@@ -2,13 +2,11 @@ let saida_resultado = document.getElementById("saida");
 let saida_dificuldade = document.getElementById("saida_dificuldade");
 let saida_tentativas = document.getElementById("tentativas_restantes");
 let saida_pontuacao = document.getElementById("saida_pontuacao");
-let saida_max_pontuacao=document.getElementById("max_pontuacao")
+let saida_max_pontuacao = document.getElementById("max_pontuacao")
 let numero_aleatorio = (Math.floor(Math.random() * 10 + 1));
 let tentativas = 1;
 let max_tentativas = Number();
 let pontuacao = 100;
-let pontuacao_rodada = pontuacao
-let pontuacao_max = 0 ;
 let resetar = document.getElementById("botao_resetar")
 let res = document.getElementById("botao_res");
 let dif = document.getElementById("botao_dif");
@@ -42,6 +40,7 @@ function dificuldade() {
 function resultado() {
     let entrada = document.getElementById("numero_digitado").value
     max_tentativas--
+    pontuacao -= 10
     if (entrada > 0 && entrada <= 10) {
         if (max_tentativas > 0) {
             saida_tentativas.innerHTML = "tentativas restantes: " + max_tentativas
@@ -58,26 +57,26 @@ function resultado() {
             saida_tentativas.innerHTML = "jogo encerrado"
             res.disabled = true
             resetar.disabled = false
-            saida_max_pontuacao.innerHTML = pontuacao_max 
+            pontuacao += 10
+            saida_pontuacao.innerHTML = pontuacao
         }
         else if (entrada > numero_aleatorio) {
             saida_resultado.innerHTML = "número é menor"
             tentativas++;
+            saida_pontuacao.innerHTML = pontuacao
         }
         else if (entrada < numero_aleatorio) {
             saida_resultado.innerHTML = "número é maior"
             tentativas++;
+            saida_pontuacao.innerHTML = pontuacao
         }
-        while (pontuacao_rodada > 0) {
-            if (pontuacao > pontuacao_max) {
-                pontu
-            }
-        }
+
     }
     else {
         saida_resultado.innerHTML = "1 a 10"
     }
 }
+
 
 function reset() {
     entrada = document.getElementById("numero_digitado")
@@ -90,5 +89,5 @@ function reset() {
     saida_resultado.innerHTML = "&nbsp"
     tentativas = 0
     tentativas++
-    saida_pontuacao.innerHTML = pontuacao_rodada = 100
+    saida_pontuacao.innerHTML = pontuacao = 100
 }
