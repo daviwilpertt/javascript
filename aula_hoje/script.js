87c5ce327f4467f25fd851cc90f3790b200cc83b
Your Name <daviwilpertt@proton.me>
2023-10-31 19:12:32 -0300

    fgfg

--- aula_hoje/script.js
+++ aula_hoje/script.js
@@ -1,18 +1,20 @@
 let saida1 = document.getElementById("saida1");
 let saida2 = document.getElementById("saida2");
-let contador = 0;
 function contar1() {
+    let contador_cres = 0;
     let reps1 = document.getElementById("numero1").value
-    while (contador < reps1) {
-        contador++
-        saida1.innerHTML += `${contador} <br>`
+    while (contador_cres < reps1) {
+        contador_cres++
+        saida1.innerHTML += `${contador_cres} <br>`
     }
     
 }
+
 function contar2() {
+    let contador_dec = 0
     let reps2 = document.getElementById("numero2").value
-    while (contador < reps2) {
-        contador--
-        saida2.innerHTML += `${contador} <br>`
+    while (reps2 > contador_dec) {
+        saida2.innerHTML += `${reps2} <br>`
+        reps2--
     }
 }
