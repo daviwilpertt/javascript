9786f39795826bfd43abb55353d794e8aeaac7b0
Your Name <daviwilpertt@proton.me>
2023-10-30 21:49:08 -0300

    jihuvjhv

--- aula_hoje/script.js
+++ aula_hoje/script.js
@@ -1,8 +1,18 @@
-function contar() {
-    let contador = 0;
-    let saida = document.getElementById("saida")
-    while (contador > 0) {
+let saida1 = document.getElementById("saida1");
+let saida2 = document.getElementById("saida2");
+let contador = 0;
+function contar1() {
+    let reps1 = document.getElementById("numero1").value
+    while (contador < reps1) {
         contador++
-        saida.innerHTML = "numero de clicks " + contador
+        saida1.innerHTML += `${contador} <br>`
+    }
+    
+}
+function contar2() {
+    let reps2 = document.getElementById("numero2").value
+    while (contador < reps2) {
+        contador--
+        saida2.innerHTML += `${contador} <br>`
     }
 }
