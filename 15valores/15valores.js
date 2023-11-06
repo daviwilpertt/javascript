ab88d6261cf226914bdb12c963cb778de0a4881a
Your Name <daviwilpertt@proton.me>
2023-11-06 20:25:52 -0300

    df

--- 15valores/15valores.js
+++ 15valores/15valores.js
@@ -2,7 +2,7 @@ let saida_maior = document.getElementById("saida_maior");
 let saida_menor = document.getElementById("saida_menor")
 let numero_maior = Number.NEGATIVE_INFINITY; 
 let numero_menor = Number.POSITIVE_INFINITY;
-for(let cont = 0; cont < 3; cont++) {
+for(let cont = 0; cont < 15; cont++) {
     let numero = Number(prompt("digite um número"));
     if (numero > numero_maior) {
         numero_maior = numero
