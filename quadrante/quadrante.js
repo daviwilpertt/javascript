ca9900954d9706c57afb15803f7432acad48a06d
Your Name <daviwilpertt@proton.me>
2023-11-07 19:22:01 -0300

    fd

--- quadrante/quadrante.js
+++ quadrante/quadrante.js
@@ -2,16 +2,21 @@ let saida = document.getElementById("saida")
 function res() {
     let valor_x = document.getElementById("valor_x").value;
     let valor_y = document.getElementById("valor_y").value;
-    if(valor_x > 0 && valor_y > 0){
-        saida.innerHTML = "primeiro quadrante"
+    if (valor_x != 0 && valor_y != 0) {
+        if (valor_x > 0 && valor_y > 0) {
+            saida.innerHTML = "primeiro quadrante"
+        }
+        else if (valor_x < 0 && valor_y > 0) {
+            saida.innerHTML = "segundo quadrante"
+        }
+        else if (valor_x < 0 && valor_y < 0) {
+            saida.innerHTML = "terceiro quadrante"
+        }
+        else if (valor_x > 0 && valor_y < 0) {
+            saida.innerHTML = "quarto quadrante"
+        }
     }
-    else if(valor_x < 0 && valor_y > 0){
-        saida.innerHTML = "segundo quadrante"
-    }
-    else if(valor_x < 0 && valor_y < 0){
-        saida.innerHTML = "terceiro quadrante"
-    }
-    else if(valor_x > 0 && valor_y < 0){
-        saida.innerHTML = "quarto quadrante"
+    else {
+        saida.innerHTML = "erro"
     }
 }
