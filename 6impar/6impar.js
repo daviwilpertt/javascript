9440420742ef0c0f156141963d5e04b59247e7cf
Your Name <daviwilpertt@proton.me>
2023-11-07 21:07:57 -0300

    hj

---
+++ 6impar/6impar.js
@@ -0,0 +1,20 @@
+let saida = document.getElementById("saida");
+let total;
+function res() {
+    let numero = Number(document.getElementById("numero").value);
+    if (numero % 2 == 0) {
+        total = numero + 1
+        for (let i = 0; i < 6; i++) {
+            saida.innerHTML += total + "<br>"
+            total += 2
+
+        }
+    }
+    else if (numero % 2 == 1) {
+        total = numero
+        for (let i = 0; i < 6; i++) {
+            saida.innerHTML += total + "<br>"
+            total += 2
+        }
+    }
+}
