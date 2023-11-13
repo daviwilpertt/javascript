e64f3b8168a7ffdb64b6ebc86bb763e6f9687263
davizok4 <daviwilpertt@proton.me>
2023-11-13 13:58:48 -0300

    ds

--- atividade s.a 2/cadastro/cadastro.js
+++ atividade s.a 2/cadastro/cadastro.js
@@ -1,3 +1,11 @@
 function voltar() {
-    window.history.go(-1)
+    window.location.href = "atividade_s.a_2/index.html"
+}
+function cadastrar() {
+    let nome = document.getElementById("nome").value;
+    let sobrenome = document.getElementById("sobrenome").value;
+    let email = document.getElementById("email").value;
+    let senha = document.getElementById("senha").value;
+    let usuario = [nome, sobrenome, email, senha]
+    console.log(usuario)
 }
