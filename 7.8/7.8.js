3720441cfee7573c35e462174e6dde0a4f65b240
Your Name <daviwilpertt@proton.me>
2023-11-13 21:55:35 -0300

    ds

--- 7.8/7.8.js
+++ 7.8/7.8.js
@@ -1,8 +1,9 @@
-let i;
+let i, j = 0;
 let saida;
-for (i = 10; i <= 1000; i++) { 
-     saida = document.write(i + "<br>")
-    if (i > 500 && i < 900) {
-        saida = document.write()
+for (i = 10; i <= 500 && j <= 1000; i++, j++) {
+    saida = document.write(i + "<br>");
+    if (i == 500) {
+        j = 900
+        saida = document.write(j + "<br>");
     }
 }
