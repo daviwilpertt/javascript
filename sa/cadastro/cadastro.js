ed33fbab1fd2e780b197adeab41c23474c5f1398
Your Name <daviwilpertt@proton.me>
2023-11-21 20:21:30 -0300

    dsd

--- sa/cadastro/cadastro.js
+++ sa/cadastro/cadastro.js
@@ -2,19 +2,29 @@
 function voltar() {
     window.location.href = "/sa/index.html"
 }
+
+
+let nome_digitado = document.getElementById("nome");
+let sobrenome_digitado = document.getElementById("sobrenome");
+let email_digitado = document.getElementById("email");
+let senha_digitada = document.getElementById("senha");
+
+let usuarios = [];
 function cadastrar() {
-    let usuarios = []
-    let nomeLido = document.getElementById("nome").value;
-    let sobrenomeLido = document.getElementById("sobrenome").value;
-    let emailLido = document.getElementById("email").value;
-    let senhaLida = document.getElementById("senha").value;
+    
+    let nomeLido = nome_digitado.value;
+    let sobrenomeLido = sobrenome_digitado.value;
+    let emailLido = email_digitado.value;
+    let senhaLida = senha_digitada.value;
+
     let usuario = {
         nome: nomeLido,
         sobrenome: sobrenomeLido,
         email: emailLido,
         senha: senhaLida
+        
     }
     
     usuarios.push(usuario);
+    console.log(usuarios);
 }
-alert(usuarios)
