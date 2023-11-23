914aa4ca4aa2e29313f3e427d340e198565e4902
Your Name <daviwilpertt@proton.me>
2023-11-23 20:45:34 -0300

    fgf

--- sa/cadastro/login.js
+++ sa/cadastro/login.js
@@ -1,20 +1,21 @@
-let usuario = JSON.parse(localStorage.getItem("usuarios"))
-console.log(usuario)
 
 
 let saida_erro = document.querySelector("#saida_erro")
 function login() {
-
+    let usuario = JSON.parse(localStorage.getItem("usuarios"))
+    
     let email = document.querySelector("#email");
     let senha = document.querySelector("#senha");
-
-
-
-    if (email == usuario[2] && senha == usuario[3]) {
-
-        window.location = "/sa/index.html";
-    }
-    else if(email != usuario[2] || senha != usuario[3]) {
-        saida_erro.innerHTML = "Email ou senha incorretos"
+    
+    for (let i = 0; i < usuario.length; i++) {
+        
+        if (email == usuario[2] && senha == usuario[3]) {
+            
+            window.location = "/sa/index.html";
+        }
+        else if (email != usuario[2] || senha != usuario[3]) {
+            saida_erro.innerHTML = "Email ou senha incorretos"
+        }
     }
 }
+console.log(usuario)
---
+++ vetores/vetor.js
@@ -0,0 +1,4 @@
+let idades = [12, 16, 20, 45];
+for (let i = 0; i < idades.length; i++){
+    console.log(idades[i]);
+}
