9689c6f7fbcdce12e2561e2b14fbda830d94f0df
Your Name <daviwilpertt@proton.me>
2023-11-23 21:55:20 -0300

    fdf

--- vetores/vetor.js
+++ vetores/vetor.js
@@ -1,13 +1,17 @@
 let louco = [];
-let input = document.querySelector("#user")
+let input = document.querySelector("#user");
+let lista = document.querySelector("#lista");
 
 function cadastrar() {
     louco.push(input.value)
     console.log(louco)
+    input.value = ""
+    listar()
 }
-for (let i = 0; i < louco.length; i++){
-    console.log([i]);
 
-
-
+function listar() {
+    for (let i = 0; i < louco.length; i++) {
+        console.log([i]);
+        lista.innerHTML = louco
+    }
 }
