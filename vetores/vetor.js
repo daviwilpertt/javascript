42ccdae3f0b43131f4ef51a5e62e8db8109048a4
Your Name <daviwilpertt@proton.me>
2023-11-27 19:58:03 -0300

    fdf

--- vetores/vetor.js
+++ vetores/vetor.js
@@ -1,6 +1,7 @@
 let louco = [];
 let input = document.querySelector("#user");
 let lista = document.querySelector("#lista");
+let pesquisa = document.querySelector("#input_pesquisa");
 
 function cadastrar() {
     louco.push(input.value)
@@ -14,4 +15,15 @@ function listar() {
         console.log([i]);
         lista.innerHTML = louco
     }
+}
+
+function pesquisa() { 
+    let pesquisado = pesquisa.value;
+    let pos_pesquisa = louco.indexOf(pesquisado);
+    alert("usuario encontrado")
+    if (pos_pesquisa != -1) {
+        pesquisa = pesquisado
+        
+    }
+
 }
