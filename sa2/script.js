321f0bb444fda9ce63fccb0c19faf25db74b4fa0
daviwilpert <122452944+daviwilpertt@users.noreply.github.com>
2023-11-28 20:14:40 -0300

    Add files via upload

---
+++ sa2/script.js
@@ -0,0 +1,25 @@
+let usuarios = JSON.parse(localStorage.getItem("usuarios"))
+console.log(usuarios)
+
+function add_viagem() {
+    window.location.href = "./add_viagem/viagem.html"
+}
+
+for (let i = 0; i < usuarios.length; i++){
+
+    
+    let nome = document.querySelector("#saida_nome");
+    nome.innerHTML = usuarios[i].nome
+}
+
+
+function sair() {
+    window.location.href = "/sa/login/pagina_login.html"
+
+
+
+}
+
+
+
+
