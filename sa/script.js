4bf3173d45389db6c4e88ddfb5973ea900a4c124
Your Name <daviwilpertt@proton.me>
2023-11-30 19:53:12 -0300

    fdf

--- sa/script.js
+++ sa/script.js
@@ -1,18 +1,21 @@
-
-function add_viagem() {
-    window.location.href = "./add_viagem/viagem.html"
-}
-
-let nome = document.querySelector("#saida_nome");
-
 let usuarios = JSON.parse(localStorage.getItem("usuarios"))
 console.log(usuarios)
 
+function add_viagem() {
+    window.location.href = "/sa/add_viagem/viagem.html"
+}
+
+for (let i = 0; i < usuarios.length; i++){
+    let nome = document.querySelector("#saida_nome");
+    nome.innerHTML = usuarios[i].nome
+}
+
 function sair() {
-    window.location.href = "/sa/cadastro/pagina_login.html"
+    window.location.href = "/sa/login/pagina_login.html"
+}
 
-
-
+function meu_perfil() {
+    window.location.href = "/sa/meu_perfil/perfil.html"
 }
 
 
